cf771641007eae82708f420d2ae81857e8ad49c4
Kaak <cours.kaak@gmail.com>
2021-08-09 23:24:08 +0200

    page html product & shopping- cart  +css

--- js/product.js
+++ js/product.js
@@ -10,16 +10,7 @@ function noWarning(){
   noLoading.style.display = 'none' ;
 }
 
-//tranfert données api choisies vers objet nounours
-function transfert(tab){
-  for (const element of tab) {
-     console.log(element) 
-    //  nounours[element] = new Nounours
-  }
-}
-// let tableau = [100,101,102,103,104,105];
-// console.log(tableau.length);
-// transfert(tableau);
+
 //  .......  tests à retirer  ........ //
 
 console.log(" test ");
@@ -30,28 +21,17 @@ console.log(` test `);
 //  .......  const  ........ //
 const noLoading = document.querySelector('.noLoading');
 const url="http://localhost:3000/api/teddies" ;
+const productId = window.location.search.substring(1);
+console.log(productId);
+
 
 //  .......  code  ........ //
 noWarning();
 
-class Nounours {
-  constructor(id,name,price,imageUrl){
-    this.id=id;
-    this.name=name;
-    this.price=price;
-    this.imageUrl=imageUrl;
-  }
-}
-
-function lesnounours(id,name,price,imageUrl) {
-    this.id=id;
-    this.name=name;
-    this.price=price;
-    this.imageUrl=imageUrl;
-}
 
 
-// var teddyTab;
+
+// new URL= location.href;
 
 
 fetch(url)
@@ -74,23 +54,22 @@ fetch(url)
       console.log(data[i].name+data[i].price); // Visualisation si la boucle est opérationnel //
       
       // Création de l'élément en HTML //
-      teddiesString +=
-             // <a href="html/product.html/:${data[i]._id}">//
-             //          <a href="html/product.html">//
+      teddiesString += 
       `<div class="teddyCard">
-      <a href="html/product.html?${data[i]._id}">
-            <figure class="ted-fig d-flex">
-              <img class="ted-img" src="${data[i].imageUrl}" alt="teddybear image" />
-              <figcaption class="ted-figcap d-flex">
-                <div class="ted-nameprice d-flex">
-                  <p class="ted-name">${data[i].name}</p>
-                  <p class="ted-price">${data[i].price/100}€</p>
-                </div>
-                <p class="ted-description">${data[i].description}</p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>`
+      <a href="../html/product.html?${data[i]._id}">
+        <figure class="ted-fig d-flex">
+          <img class="ted-img" src="${data[i].imageUrl}" alt="teddybear image" />
+          <figcaption class="ted-figcap d-flex">
+            <div class="ted-nameprice d-flex">
+              <p class="ted-name">${data[i].name}</p>
+              <p class="ted-price">${data[i].price/100}€</p>
+            </div>
+            <p class="ted-description">${data[i].description}</p>
+
+          </figcaption>
+        </figure>
+      </a>
+    </div>`
     };
     
      // Insertion des éléments recuperés dans la page index.html //
