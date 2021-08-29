c25229a0a68b908ff01018c9f5d1fe2271d4b82f
Kaak <cours.kaak@gmail.com>
2021-08-29 16:33:05 +0200

    essai de json à envoyer et de fetch-post :  fail(error400)

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -37,3 +37,66 @@ noWarning();
 
 //sauver d'emblée le nouveau choix et  APRÈS afficher tout
 //stocker avec index ?json?
+
+//tabCart=[];
+
+//objet de contact 
+let objContact={
+  firstName: "ka",
+  lastName: "ak",
+  address: "123 azerty",
+  city: "laville",
+  email: "cours@gg.com"
+};
+console.log(objContact);
+
+//tableau de produits  
+
+let productTab=[
+  {
+  name: "Arnold",
+  price: 39,
+  color: "chocolate",
+  number: 3,
+  },
+  {
+    name: "Gustav",
+    price: 45,
+    color: "white",
+    number: 2,
+  }
+];
+
+console.log(productTab);
+
+let jsonOrder=JSON.stringify({ objContact, productTab });
+console.log(jsonOrder);
+
+fetch("http://localhost:3000/api/teddies/order",
+ {method: "POST", headers:{"Content-Type": "application/json"}, body: jsonOrder} )
+  .then(res => {
+    if (res.ok) {
+      console.log("success(fetch url)!");
+      return res.json();
+    } else {
+      console.log("failed (fetch url)!")
+      warning();
+    }
+  })
+  .then(data => {
+    localStorage.setItem("order", JSON.stringify(data));
+    // document.location.href = "my-orders.html";
+});
+
+// fetch("http://localhost:3000/api/teddies/order", {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-Type": "application/json",
+//                 },
+//                 body: JSON.stringify({ objContact, productTab }),
+//             })
+//                 .then((response) => response.json())
+//                 .then((data) => {
+//                     localStorage.setItem("order", JSON.stringify(data));
+//                     document.location.href = "my-orders.html";
+//                 });
